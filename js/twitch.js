f17aca5b1a0626169770bbffa8b18a0968d6cc02
Promie Yutasane <promie@greenguys.local>
2017-11-16 10:45:32 +1100

    2nd API Working

--- js/twitch.js
+++ js/twitch.js
@@ -1,11 +1,22 @@
 $(document).ready(function(){
 
-    const streamers = ['ESL_SC2', 'OgamingSC2', 'cretetion', 'freecodecamp', 'storbeck', 'habathcx', 'RobotCaleb', 'noobs2ninjas', 'septiess', 'vihart', 'frinlet', 'Dr4xell', 'ExtremeModeration', 'eisighul', 'SYNTAG', 'MeteorDev', 'texEira'];
- 
+    const streamers = ['ESL_SC2', 'OgamingSC2', 'cretetion', 'freecodecamp', 'storbeck', 'habathcx', 'RobotCaleb', 'noobs2ninjas', 'septiess', 'vihart', 'frinlet', 'Dr4xell', 'ExtremeModeration', 'eisighul', 'SYNTAG', 'MeteorDev'];
+    const noImageURL = 'http://www.iconninja.com/files/791/598/492/humans-person-social-users-profile-friends-account-icon.svg';
+
+
     streamers.forEach(streamer =>{
         $.getJSON(getStatus(streamer), data=>{
             
-            //API for status
+
+            console.log(data.display_name);
+
+            /*
+            if(data.stream !== null){
+                $(`#${data.channel.display_name}`)
+
+            }
+
+            */
             
         });
     });
@@ -13,9 +24,12 @@ $(document).ready(function(){
     streamers.forEach(streamer =>{
         $.getJSON(getChannelInfo(streamer), data2=>{
 
-            $('.')
-          
-            
+            if(data2.logo === null){
+                $('.offline-streamer').append(`<div id="${data2.display_name}" class="streamer"><a href="${data2.url}"><img class="logo" src="${noImageURL}"></a><p class="streamerName">${data2.display_name}</p><p class="game">Offline</p></div>`)
+            }else{
+                $('.offline-streamer').append(`<div id="${data2.display_name}" class="streamer"><a href="${data2.url}"><img class="logo" src="${data2.logo}"></a><p class="streamerName">${data2.display_name}</p><p class="game">Offline</p></div>`)
+                console.log(data2);
+            }
         });
     });
 
