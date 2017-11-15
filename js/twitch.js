4b374ef3b241bdf8aa52decd3048497c75db174d
Promie Yutasane <promie@greenguys.local>
2017-11-16 10:03:17 +1100

    Initial Design updated

--- js/twitch.js
+++ js/twitch.js
@@ -1,23 +1,3 @@
-// First Part --> Get Free Code Camp Status and Hyperlink use the $.ajax() call
-
-
-//Check whether the free code camp is online or offline
-// API for Free Code Camp --> https://wind-bow.glitch.me/twitch-api/streams/freecodecamp
-
-
-// Second Part --> Get the followers status and hyper link
-
-//API for Free Code Camp Followers --> https://wind-bow.glitch.me/twitch-api/users/freecodecamp/follows/channels
-
-// Return the display name of the followers and put push it into an new array
-
-// https://wind-bow.glitch.me/twitch-api/channels/[list of Streamers]
-
-//Loop through that new array and use call API the third time to loop through the new array to get the logo, info and status
-
-// Third API call --> https://wind-bow.glitch.me/twitch-api/streams/[followersname]/?callback=?;
-
-
 $(document).ready(function(){
 
     const streamers = ['ESL_SC2', 'OgamingSC2', 'cretetion', 'freecodecamp', 'storbeck', 'habathcx', 'RobotCaleb', 'noobs2ninjas', 'septiess', 'vihart', 'frinlet', 'Dr4xell', 'ExtremeModeration', 'eisighul', 'SYNTAG', 'MeteorDev', 'texEira'];
@@ -25,13 +5,7 @@ $(document).ready(function(){
     streamers.forEach(streamer =>{
         $.getJSON(getStatus(streamer), data=>{
             
-            if(data.stream === null){
-                $('#results').append(`<p class="offline">Offline</p>
-                                    <p class="streamer">${streamer}</p>`);
-            }else{
-                $('#results').append(`<p class="online">Online</p>
-                                    <p class="streamer">${streamer}</p>`);
-            }
+            //API for status
             
         });
     });
@@ -39,6 +13,7 @@ $(document).ready(function(){
     streamers.forEach(streamer =>{
         $.getJSON(getChannelInfo(streamer), data2=>{
 
+            $('.')
           
             
         });
@@ -61,7 +36,6 @@ const getChannelInfo = (streamersName) =>{
         return `${streamersURL}${streamersName}`
     
 }
-    
 
 
 
