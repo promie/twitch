b881d215f07ab0ce0632b13548c8185f68e4296e
Promie Yutasane <promie.yutasane@gmail.com>
2017-11-16 19:41:09 +1100

    Code Clean Up

--- js/twitch.js
+++ js/twitch.js
@@ -1,57 +1,59 @@
 $(document).ready(function(){
 
+    init();
+
+});
+
+const init = () =>{
+
     const streamers = ['ESL_SC2', 'OgamingSC2', 'cretetion', 'freecodecamp', 'storbeck', 'habathcx', 'RobotCaleb', 'noobs2ninjas', 'septiess', 'vihart', 'frinlet', 'Dr4xell', 'eisighul', 'SYNTAG', 'MeteorDev'];
     const noImageURL = 'http://www.iconninja.com/files/791/598/492/humans-person-social-users-profile-friends-account-icon.svg';
 
     clickEvents();
 
     streamers.forEach(streamer =>{
-        $.getJSON(getChannelInfo(streamer), data=>{
-
+        $.getJSON(getChannelInfo('channels', streamer), data=>{
             channelStatus(data);
-
-            if(data.logo === null){
-                $('.offline-streamer').append(`<div id="${data.display_name}" class="streamer"><a href="${data.url}" target="_blank"><img class="logo" src="${noImageURL}"></a><p class="streamerName">${stringFormat(data.display_name)}</p><p class="game"><i class="fa fa-ban iconFormat" aria-hidden="true"></i></p></div>`)
-            }else{
-                $('.offline-streamer').append(`<div id="${data.display_name}" class="streamer"><a href="${data.url}" target="_blank"><img class="logo" src="${data.logo}"></a><p class="streamerName">${stringFormat(data.display_name)}</p><p class="game"><i class="fa fa-ban iconFormat" aria-hidden="true"></i></p></div>`)
-            }
+            displayInformation(data);
         });
     });
-});
+
+}
 
 const channelStatus = (data) =>{
     const name = data.display_name;
 
-    $.getJSON(getStatus(name), data=>{
+    $.getJSON(getChannelInfo('streams', name), data=>{
         
         if(data.stream !== null){
-            console.log(data.stream.channel.url);
             $(`#${name}`).remove();
             $('.online-streamer').append(`<div class="streamer online"><a href="${data.stream.channel.url}" target="_blank"><img class="logo" src="${data.stream.channel.logo}"</a><p class="streamerName">${stringFormat(name)}</p><p class="game">Streaming: ${stringFormat(data.stream.game)}</p></div>`);
         }
     });
 }
 
-const getStatus = (streamersName) =>{
-
-    const streamersURL = 'https://wind-bow.glitch.me/twitch-api/streams/';
-
-    return `${streamersURL}${streamersName}`
+const displayInformation = (data) =>{
+    
+    if(data.logo === null){
+        $('.offline-streamer').append(`<div id="${data.display_name}" class="streamer"><a href="${data.url}" target="_blank"><img class="logo" src="${noImageURL}"></a><p class="streamerName">${stringFormat(data.display_name)}</p><p class="game"><i class="fa fa-ban iconFormat" aria-hidden="true"></i></p></div>`)
+    }else{
+        $('.offline-streamer').append(`<div id="${data.display_name}" class="streamer"><a href="${data.url}" target="_blank"><img class="logo" src="${data.logo}"></a><p class="streamerName">${stringFormat(data.display_name)}</p><p class="game"><i class="fa fa-ban iconFormat" aria-hidden="true"></i></p></div>`)
+    }
 
 }
 
-const getChannelInfo = (streamersName) =>{
+const getChannelInfo = (type, streamersName) =>{
     
-        const streamersURL = 'https://wind-bow.glitch.me/twitch-api/channels/';
+        const streamersURL = 'https://wind-bow.glitch.me/twitch-api/';
     
-        return `${streamersURL}${streamersName}`
+        return `${streamersURL}${type}/${streamersName}`
 }
 
 const stringFormat = (text) =>{
-    
-    
+
     return `<strong>${text}</strong>`;
 }
+
 
 const clickEvents = () =>{
 
