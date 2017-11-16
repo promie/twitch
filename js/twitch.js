d0fce271e1ad624061b2b8a332b823846d926994
Promie Yutasane <promie@greenguys.local>
2017-11-16 16:30:58 +1100

    Glyphicon addded

--- js/twitch.js
+++ js/twitch.js
@@ -3,15 +3,17 @@ $(document).ready(function(){
     const streamers = ['ESL_SC2', 'OgamingSC2', 'cretetion', 'freecodecamp', 'storbeck', 'habathcx', 'RobotCaleb', 'noobs2ninjas', 'septiess', 'vihart', 'frinlet', 'Dr4xell', 'eisighul', 'SYNTAG', 'MeteorDev'];
     const noImageURL = 'http://www.iconninja.com/files/791/598/492/humans-person-social-users-profile-friends-account-icon.svg';
 
+    clickEvents();
+
     streamers.forEach(streamer =>{
         $.getJSON(getChannelInfo(streamer), data=>{
 
             channelStatus(data);
 
             if(data.logo === null){
-                $('.offline-streamer').append(`<div id="${data.display_name}" class="streamer"><a href="${data.url}" target="_blank"><img class="logo" src="${noImageURL}"></a><p class="streamerName">${data.display_name}</p><p class="game">Offline</p></div>`)
+                $('.offline-streamer').append(`<div id="${data.display_name}" class="streamer"><a href="${data.url}" target="_blank"><img class="logo" src="${noImageURL}"></a><p class="streamerName">${stringFormat(data.display_name)}</p><p class="game"><span class="glyphicon glyphicon-remove-sign iconFormat" aria-hidden="true"></span></p></div>`)
             }else{
-                $('.offline-streamer').append(`<div id="${data.display_name}" class="streamer"><a href="${data.url}" target="_blank"><img class="logo" src="${data.logo}"></a><p class="streamerName">${data.display_name}</p><p class="game">Offline</p></div>`)
+                $('.offline-streamer').append(`<div id="${data.display_name}" class="streamer"><a href="${data.url}" target="_blank"><img class="logo" src="${data.logo}"></a><p class="streamerName">${stringFormat(data.display_name)}</p><p class="game"><span class="glyphicon glyphicon-remove-sign iconFormat" aria-hidden="true"></span></p></div>`)
             }
         });
     });
@@ -35,7 +37,7 @@ const channelStatus = (data) =>{
         if(data.stream !== null){
             console.log(data.stream.channel.url);
             $(`#${name}`).remove();
-            $('.online-streamer').append(`<div class="streamer online"><a href="${data.stream.channel.url}" target="_blank"><img class="logo" src="${data.stream.channel.logo}"</a><p class="streamerName">${name}</p><p class="game">${data.stream.game}</p></div>`);
+            $('.online-streamer').append(`<div class="streamer online"><a href="${data.stream.channel.url}" target="_blank"><img class="logo" src="${data.stream.channel.logo}"</a><p class="streamerName">${stringFormat(name)}</p><p class="game">Streaming: ${stringFormat(data.stream.game)}</p></div>`);
         }
     });
 
@@ -57,21 +59,31 @@ const getChannelInfo = (streamersName) =>{
     
 }
 
+const stringFormat = (text) =>{
+    
+    
+    return `<strong>${text}</strong>`;
+}
 
-$('.all-button').on('click', function(){
-    $('.online-streamer').show(1000)
-    $('.offline-streamer').show(1000)
-});
+const clickEvents = () =>{
 
-$('.online-button').on('click', function(){
-    $('.offline-streamer').hide(1000)
-    $('.online-streamer').show(1000)        
-});
+    $('#all-button').on('click', function(){
+        $('.online-streamer').removeClass('hide')
+        $('.offline-streamer').removeClass('hide')
+    });
+    
+    $('#online-button').on('click', function(){
+        $('.offline-streamer').addClass('hide')
+        $('.online-streamer').removeClass('hide')        
+    });
+    
+    $('#offline-button').on('click', function(){
+        $('.online-streamer').addClass('hide')
+        $('.offline-streamer').removeClass('hide')
+    });
+    
+}
 
-$('.offline-button').on('click', function(){
-    $('.online-streamer').hide(1000)
-    $('.offline-streamer').show(1000)
-});
 
 
 
